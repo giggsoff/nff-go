dde224601cd73b78325ad80c16bf02459f957d46
Petr Fedchenkov <giggsoff@gmail.com>
2019-12-09 18:11:31 +0300

    crypt

--- examples/tunnel/utils/balancer.go
+++ examples/tunnel/utils/balancer.go
@@ -7,8 +7,39 @@ package tunnel
 import (
 	"github.com/intel-go/nff-go/flow"
 	"github.com/intel-go/nff-go/packet"
+	"github.com/intel-go/nff-go/types"
 )
 
-func balancer(pkt *packet.Packet, ctx flow.UserContext) bool {
+const espHeadLen = 36
+const etherLen = types.EtherLen
+const outerIPLen = types.IPv4MinLen
+
+type cryptHeader struct {
+	SEQ uint32
+	IV  [16]byte
+	KEY [16]byte
+}
+
+func balancer(currentPacket *packet.Packet, context0 flow.UserContext) bool {
+	context := (context0).(*SContext)
+	length := currentPacket.GetPacketLen()
+	paddingLength := uint8((16 - (length-(etherLen+outerIPLen+espHeadLen))%16) % 16)
+	newLength := length + uint(paddingLength)
+	currentPacket.GetIPv4NoCheck().TotalLength = packet.SwapBytesUint16(uint16(newLength) - etherLen)
+	currentPacket.EncapsulateTail(length, uint(paddingLength))
+
+	currentESPHeader := (*cryptHeader)(currentPacket.StartAtOffset(etherLen + outerIPLen))
+	currentESPHeader.KEY = [16]byte{0x90, 0x9d, 0x78, 0xa8, 0x72, 0x70, 0x68, 0x00, 0x8f, 0xdc, 0x55, 0x73, 0xa3, 0x75, 0xb5, 0xa7}
+	currentESPHeader.IV = [16]byte{0x90, 0x9d, 0x78, 0xa8, 0x72, 0x70, 0x68, 0x00, 0x8f, 0xdc, 0x55, 0x73, 0xa3, 0x75, 0xb5, 0xa7}
+
+	// Encryption
+	EncryptionPart := (*[types.MaxLength]byte)(currentPacket.StartAtOffset(0))[etherLen+outerIPLen+espHeadLen : newLength]
+	context.modeEnc.(SetIVer).SetIV(currentESPHeader.IV[:])
+	context.modeEnc.CryptBlocks(EncryptionPart, EncryptionPart)
+
+	// Authentication
+	context.mac123.Reset()
+	AuthPart := (*[types.MaxLength]byte)(currentPacket.StartAtOffset(0))[etherLen+outerIPLen : newLength]
+	context.mac123.Write(AuthPart)
 	return true
 }
---
+++ examples/tunnel/utils/crypt.go
@@ -0,0 +1,40 @@
+package tunnel
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/sha1"
+	"hash"
+)
+
+type SContext struct {
+	mac123  hash.Hash
+	modeEnc cipher.BlockMode
+	modeDec cipher.BlockMode
+}
+
+type SetIVer interface {
+	SetIV([]byte)
+}
+
+func InitSContext() interface{} {
+	var auth123Key = []byte("qqqqqqqqqqqqqqqqqqqq")
+	var crypt123Key = []byte("AES128Key-16Char")
+	block123, _ := aes.NewCipher(crypt123Key)
+
+	tempScalarIV := make([]byte, 16)
+
+	n := new(SContext)
+	n.mac123 = hmac.New(sha1.New, auth123Key)
+	n.modeEnc = cipher.NewCBCEncrypter(block123, tempScalarIV)
+	n.modeDec = cipher.NewCBCDecrypter(block123, tempScalarIV)
+	return n
+}
+
+func (c SContext) Copy() interface{} {
+	return InitSContext()
+}
+
+func (c SContext) Delete() {
+}
